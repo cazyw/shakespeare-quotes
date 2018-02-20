70de2df285e2721d8785233cbb9689313bfdf72d
Caz <cazyw@users.noreply.github.com>
2018-02-20 22:31:19 +1100

    Include config file

---
+++ config.js
@@ -0,0 +1,21 @@
+const config = {
+  production: {
+    database: `${process.env.MONGODB_URI}`
+  },
+
+  development: {
+    database: 'mongodb://localhost/shakespeare'
+  },
+
+  test: {
+    database: 'mongodb://localhost/testDatabase'
+  },
+
+  default: {
+    database: 'mongodb://localhost/shakespeare'
+  }
+}
+
+exports.get = function get(env) {
+  return config[env] || config.default;
+}
