03f9f4b33504c299d16fbcaf53bd5fa939741ba4
cazyw <kazeisc@gmail.com>
2018-08-07 00:41:40 +1000

    check whether the div ID exists before opening/closing the section

--- client/src/utils/updateDisplay.js
+++ client/src/utils/updateDisplay.js
@@ -1,14 +1,18 @@
 import { TIMEOUT } from './constants';
 
 export const openElement = (sectionToOpen) => {
-  document.getElementById(sectionToOpen).classList.add('open');
+  if(document.getElementById(sectionToOpen) !== null){
+    document.getElementById(sectionToOpen).classList.add('open');
+  }
 };
 
 export const closeElements = (...sectionsToClose) => {
   let sectionsClosed = false;
   sectionsToClose.forEach(section => {
-    sectionsClosed = sectionsClosed || (document.getElementById(section).classList.contains('open') ? true : false);
-    document.getElementById(section).classList.remove('open');
+    if(document.getElementById(section) !== null){
+      sectionsClosed = sectionsClosed || (document.getElementById(section).classList.contains('open') ? true : false);
+      document.getElementById(section).classList.remove('open');
+    }
   });
   return sectionsClosed;
 };
