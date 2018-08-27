29eeedd5cd4e9d86311b752390a6bda836dad809
cazyw <kazeisc@gmail.com>
2018-08-28 00:03:09 +1000

    Retrieve list of links to each of shakespeare's works from the Shakespeare MIT page

--- mit-shakespeare/retrieve-works.js
+++ mit-shakespeare/retrieve-works.js
@@ -1,7 +1,9 @@
+const pupeteer = require('puppeteer');
 const fs = require('fs');
 const originalDir = './originalWorks';
 const modifiedDir = './modifiedWorks';
 
+// folders for original and modified works
 if (!fs.existsSync(originalDir)){
   fs.mkdirSync(originalDir);
 }
@@ -9,3 +11,33 @@ if (!fs.existsSync(originalDir)){
 if (!fs.existsSync(modifiedDir)){
   fs.mkdirSync(modifiedDir);
 }
+
+// retrieve links for shakespeare's works
+let shakespeareWorksLinks = async () => {
+  const browser = await pupeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('http://shakespeare.mit.edu/');
+  await page.waitFor(1000);
+
+  const result = await page.evaluate(() => {
+    const works = [];
+    let links = document.querySelectorAll('a');
+    let isAWork = false;
+    for(let i = 2; i < links.length; i++) {
+      isAWork = links[i].href.includes('allswell') || links[i].href.includes('python') ? !isAWork : isAWork ;
+      if(isAWork){
+        works.push(links[i].href);
+      }
+    }
+    return works;
+  });
+
+  await browser.close();
+  return result;
+};
+
+shakespeareWorksLinks().then((output) => {
+  /* eslint-disable no-console */
+  console.log(output);
+});
+
