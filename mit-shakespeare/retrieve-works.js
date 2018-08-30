2164341d73edb998dabe7248801637d3289896df
Caz <cazyw@users.noreply.github.com>
2018-08-30 23:30:04 +1000

    add function to extract the play or poetry's name from the list of works and create the url of the full text of the work

--- mit-shakespeare/retrieve-works.js
+++ mit-shakespeare/retrieve-works.js
@@ -29,7 +29,6 @@ let puppeteerTeardown = async (browser) => {
 let shakespeareWorksLinks = async () => {
   const { browser, page } = await puppeteerSetup();
   await page.goto('http://shakespeare.mit.edu/', { waitUntil: 'networkidle2' });
-  // await page.waitFor(1000);
 
   const result = await page.evaluate(() => {
     const works = [];
@@ -45,31 +44,43 @@ let shakespeareWorksLinks = async () => {
   });
 
   await puppeteerTeardown(browser);
+  // console.log(result);
   return result;
 };
 
-let downloadPage = async (url) => {
+let processLink = (url) => {
+  var regexPlay = /edu\/(.+)\//i;
+  var regexPoetry = /edu\/Poetry\/(.+)\.html/i;
+  const isPoetry = url.match(regexPoetry);
+  const workName = isPoetry === null ? url.match(regexPlay)[1] : url.match(regexPoetry)[1];
+  const fullUrl = url.replace('index.html', 'full.html');
+  return { workName, fullUrl };
+};
+
+shakespeareWorksLinks().then((listOfWorks) => {
+  listOfWorks.forEach((work) => {
+    processLink(work);
+  });
+});
+
+let downloadAPage = async (url) => {
   const { browser, page } = await puppeteerSetup();
   await page.goto(url, { waitUntil: 'networkidle2' });
   const html = await page.content();
   const filename = `${originalDir}/allswell.html`;
   fs.writeFileSync(filename, html, function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log(`Saved ${filename}!`);
   });
   await puppeteerTeardown(browser);
   return html;
 };
 
-// downloadPage('http://shakespeare.mit.edu/allswell/full.html').then((output) => {
+// downloadAPage('http://shakespeare.mit.edu/allswell/full.html').then((output) => {
 //   console.log(output);
 // });
 
 
-// shakespeareWorksLinks().then((output) => {
-//   /* eslint-disable no-console */
-//   console.log(output);
-// });
 
 module.exports = {
   shakespeareWorksLinks
