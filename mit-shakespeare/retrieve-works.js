eac9544645201336962c154162f66199092539ff
Caz <cazyw@users.noreply.github.com>
2018-09-07 22:53:32 +1000

    fix broken tests  - number of expected works and link processing

--- mit-shakespeare/retrieve-works.js
+++ mit-shakespeare/retrieve-works.js
@@ -4,24 +4,22 @@ const fs = require('fs');
 const util = require('util');
 const fs_writeFile = util.promisify(fs.writeFile);
 const { asyncForEach } = require('./asyncHelper');
-const { createOriginalFolder, createModifiedFolder } = require('./folderHelper');
+const { createOriginalFolder } = require('./folderHelper');
 const originalDir = './originalWorks';
 // const modifiedDir = './modifiedWorks';
 
-let browser;
-let page;
-
 const puppeteerSetup = async () => {
-  browser = await pupeteer.launch({headless: false});
-  page = await browser.newPage();
+  const browser = await pupeteer.launch({headless: false});
+  const page = await browser.newPage();
+  return { browser, page };
 };
 
-const puppeteerTeardown = async () => {
+const puppeteerTeardown = async (browser) => {
   await browser.close();
 };
 
 // get list of links for shakespeare's works from http://shakespeare.mit.edu/
-const getShakespeareWorksLinks = async () => {
+const getShakespeareWorksLinks = async (page) => {
   await page.goto('http://shakespeare.mit.edu/');
   await page.waitFor(1000);
   const result = await page.evaluate(() => {
@@ -42,14 +40,15 @@ const processLink = (url) => {
   var regexPlay = /edu\/(.+)\//i;
   var regexPoetry = /edu\/Poetry\/(.+)\.html/i;
   const isPoetry = url.match(regexPoetry);
-  const workName = isPoetry ? url.match(regexPoetry)[1] : url.match(regexPlay)[1];
+  const isPlay = url.match(regexPlay);
+  const workName = isPoetry ? url.match(regexPoetry)[1] : isPlay ? url.match(regexPlay)[1] : null;
   const fullUrl = url.replace('index.html', 'full.html');
   console.log('work: ', workName, ' == ', fullUrl);
   return { workName, fullUrl };
 };
 
 // download the full text of a piece of work
-const downloadAPage = async (work, url) => {
+const downloadAPage = async (page, work, url) => {
   await page.goto(url);
   await page.waitFor(5000);
   const html = await page.content();
@@ -58,7 +57,7 @@ const downloadAPage = async (work, url) => {
   console.log(`Saved ${filename}!`);
 };
 
-const downloadSonnets = async (sonnetUrl) => {
+const downloadSonnets = async (page, sonnetUrl) => {
   await puppeteerSetup();
   console.log('Retrieving sonnets');
   await page.goto(sonnetUrl);
@@ -81,17 +80,17 @@ const downloadSonnets = async (sonnetUrl) => {
   await puppeteerTeardown(browser);
 };
 
-downloadSonnets('http://shakespeare.mit.edu/Poetry/sonnets.html');
+// downloadSonnets('http://shakespeare.mit.edu/Poetry/sonnets.html');
 
 // get the full list of works and download them all
 // there are 42 plays and 154 sonnets
 const downloadAllPages = async () => {
-  await puppeteerSetup();
-  const listOfWorks = await getShakespeareWorksLinks();
+  const {browser, page } = await puppeteerSetup();
+  const listOfWorks = await getShakespeareWorksLinks(page);
   createOriginalFolder();
   await asyncForEach(listOfWorks, async (work) => {
     const { workName, fullUrl } = processLink(work);
-    await downloadAPage(workName, fullUrl);
+    await downloadAPage(page, workName, fullUrl);
     if (workName === 'sonnets') downloadSonnets(fullUrl);
   });
   await puppeteerTeardown(browser);
@@ -100,6 +99,9 @@ const downloadAllPages = async () => {
 // downloadAllPages();
 
 module.exports = {
-  getShakespeareWorksLinks
+  getShakespeareWorksLinks,
+  puppeteerSetup,
+  puppeteerTeardown,
+  processLink
 };
 
