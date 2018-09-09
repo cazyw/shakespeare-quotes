5355aa2929b54bf56b7f2b5796b345ff4e07a287
cazyw <kazeisc@gmail.com>
2018-09-09 21:51:04 +1000

    add function to remove all html tags

---
+++ mit-shakespeare/parseHTML.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const util = require('util');
+const sanitizeHtml = require('sanitize-html');
+const fs_writeFile = util.promisify(fs.writeFile);
+const originalDir = './originalWorks';
+const modifiedDir = './modifiedWorks';
+
+const removeHtmlTags = (file) => {
+  fs.readFile(`./${originalDir}/${file}`, (err, data) => {
+    if (err) throw err;
+    const cleanHtml = sanitizeHtml(data, {
+      allowedTags: [],
+      allowedAttributes: []
+    });
+    fs_writeFile(`./${modifiedDir}/${file}`, cleanHtml);
+  });
+};
+
+removeHtmlTags('1henryiv.html');
+
+
